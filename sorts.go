d15e2e644daf32fa4479b55a39bc752662ec15ef
Charlie Krajewski <onanysunday@yahoo.com>
2021-03-13 22:48:45 -0500

    End of day 3-13-2021

--- sorts.go
+++ sorts.go
@@ -1,6 +1,26 @@
 package main
 
-func Sortie() {
-	x := 0
-	x *= 3
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+	k := (records[0][0])
+	fmt.Println("k = ", k)
+
+	return records
 }
