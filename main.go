144fdcd46a8a91a99a34d1ba0c52ceacdf7d0ca1
Charlie Krajewski <onanysunday@yahoo.com>
2021-03-14 18:33:26 -0400

    debugged readCsvFile

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Chas Yo")
 	x := Multp()
 	fmt.Println("x= ", x)
-	k := readCsvFile("C:/Users/onany/OneDrive/Documents/krud/sort.csv")
+	k := readCsvFile("C:/Users/onany/OneDrive/Documents/krud/sort.csv", 1)
 	fmt.Println("k = ", k)
 
 }
--- sorts.go
+++ sorts.go
@@ -7,17 +7,24 @@ import (
 	"os"
 )
 
-func readCsvFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
+func readCsvFile(filepath string, skiplines int) [][]string {
+	f, err := os.Open(filepath)
 	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
+		log.Fatal("Unable to read input file "+filepath, err)
 	}
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
+	for i := 0; i < skiplines; i++ {
+		topline, err1 := csvReader.Read()
+		topline = topline
+		if err1 != nil {
+			log.Fatal("Could not read top line "+filepath, err1)
+		}
+	}
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+		log.Fatal("Unable to parse file as CSV for "+filepath, err)
 	}
 	k := (records[0][0])
 	fmt.Println("k = ", k)
