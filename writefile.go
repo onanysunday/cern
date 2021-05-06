12d918356ddcac4eae82c70850a319aa99de69e1
Charlie Krajewski <onanysunday@yahoo.com>
2021-05-05 23:13:46 -0400

    5/5/2021

--- writefile.go
+++ writefile.go
@@ -9,11 +9,19 @@ import (
 )
 
 func WriteCsvFile(mat [][]float32, filepath string, comma rune) error {
-	f, err := os.Open(filepath)
+	f, err := os.Create(filepath)
 	if err != nil {
 		log.Fatal("Unable to read input file "+filepath, err)
 	}
 	defer f.Close()
+
+	lth, err := f.WriteString("Hello World")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return err
+	}
+	fmt.Println("File Length = ", lth)
 
 	fmt.Println("Done Writing File")
 	return nil
